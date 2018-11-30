427c64ff4ba9149b54e9db2a897749d46f4cd2de
DimitarToshevski <dimitar_toshevski@abv.bg>
2018-12-01 00:23:46 +0200

    Setup the project

---
+++ youth-centre/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './app-layout/header/header.component';
+import { FooterComponent } from './app-layout/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
