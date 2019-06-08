1c787aeef932f3d6135f3a27341becc501c9060c
war_master@bitex.bg <dimitar.toshevski@mentormate.com>
2019-06-08 12:38:52 +0300

    Added settings scss paths to stylePreprocessorOptions, Added shared path to shared folder

--- youth-centre/src/app/app-layout/header/navigation-menu/navigation-menu.content.ts
+++ youth-centre/src/app/app-layout/header/navigation-menu/navigation-menu.content.ts
@@ -1,9 +1,9 @@
 export const menuItems = [
-  { title: 'НАЧАЛО', link: '/home'},
-  { title: 'ЗА НАС', link: '/about'},
-  { title: 'НОВИНИ', link: '/news'},
-  { title: 'ДЕЙНОСТИ', link: '/activities' },
-  { title: 'ПРЕДСТОЯЩИ СЪБИТИЯ', link: '/events'},
-  { title: 'ЕКИП', link: '/team'},
-  { title: 'КОНТАКТИ', link: '/contact'},
+  { title: "НАЧАЛО", link: "/home" },
+  { title: "ЗА НАС", link: "/about" },
+  { title: "НОВИНИ", link: "/news" },
+  { title: "ДЕЙНОСТИ", link: "/activities" },
+  { title: "СЪБИТИЯ", link: "/events" },
+  { title: "ЕКИП", link: "/team" },
+  { title: "КОНТАКТИ", link: "/contact" }
 ];
